5c029a38d2874fb73d74d5df5b8c02c78da2ea94
Ruben Beglaryan <beglaryan@flagbit.de>
2019-02-01 00:47:21 +0400

    cs fix

--- src/Controller/AbstractController.php
+++ src/Controller/AbstractController.php
@@ -10,16 +10,15 @@ use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
 abstract class AbstractController extends Controller
 {
 
-    protected abstract function getNormalizer(): NormalizerInterface;
-    protected abstract function getAttributeRepository(): AttributeRepositoryInterface;
+    abstract protected function getNormalizer(): NormalizerInterface;
+    abstract protected function getAttributeRepository(): AttributeRepositoryInterface;
 
     protected function normalizeProduct(EntityWithFamilyVariantInterface $product)
     {
         $normalizedProduct = $this->getNormalizer()->normalize($product, 'standard');
 
         while ($parent = $product->getParent()) {
-            foreach ($parent->getValuesForVariation() as $value)
-            {
+            foreach ($parent->getValuesForVariation() as $value) {
                 //this workaround removes the attributes of all parent models, as the getValues() Method,
                 // which is called by the normalizer, returns all Values including the values of the parent Model
                 unset($normalizedProduct['values'][$value->getAttribute()->getCode()]);
